81de6548e4bf704e3969bc26594e2aee904082bf
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-12-16 17:20:26 +0700

    add void remove

--- Sample Circular Queues/Program.cs
+++ Sample Circular Queues/Program.cs
@@ -38,6 +38,27 @@ namespace CircularQueues_CSharp
             /*Once the position of REAR is determined, the element is added at its proper place. */
             queue_array[REAR] = element;
         }
+        public void remove()
+        {
+            /*Checks whether the queue is empty. */
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue underflow\n");
+                return;
+            }
+            Console.WriteLine("\nThe element deleted from the queue is: " + queue_array[FRONT] + "\n");
+            /*Checks if the queue has one element.*/
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+                /*If the element to be deleted is at the last position of the array, then value of FRONT is set to 0 i.e to the first element of the array. */
+                FRONT = FRONT + 1;
+            }
+        }
 
     }
 }
